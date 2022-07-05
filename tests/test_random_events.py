7968a13dde9120bd7c3848db705e3e33e8341b8f
0xfinder <0xfinder@protonmail.com>
2022-07-05 22:09:58 +0800

    added formatter (black)

--- tests/test_random_events.py
+++ tests/test_random_events.py
@@ -1,27 +1,21 @@
 import pytest
 from dank_memer.helper import kraken, dragon
 
-@pytest.mark.random_events_kraken
 def test_kraken_0():
     assert kraken(kraken_text[0]) == 2
 
-@pytest.mark.random_events_kraken
 def test_kraken_1():
     assert kraken(kraken_text[1]) == 0
 
-@pytest.mark.random_events_kraken
 def test_kraken_2():
     assert kraken(kraken_text[2]) == 1
 
-@pytest.mark.random_events_dragon
 def test_dragon_0():
     assert dragon(dragon_text[0]) == 0
 
-@pytest.mark.random_events_dragon
 def test_dragon_1():
     assert dragon(dragon_text[1]) == 0
 
-@pytest.mark.random_events_dragon
 def test_dragon_2():
     assert dragon(dragon_text[2]) == 0
 
---
+++ tests/work/test_emoji_match.py
@@ -0,0 +1,15 @@
+from dank_memer.work import emoji_match
+from discord import Button, Message, Component, ActionRow
+
+old_message = Message()
+old_message.contents = """Work for Discord Mod - Emoji Match - Look at the emoji closely!
+:thinking:"""
+
+new_message = Message()
+new_message.contents = """What was the emoji?"""
+new_message.components = [ActionRow()]
+new_message.components[0].children = [Button(emoji="")]
+
+
+def test_emoji_match():
+    assert emoji_match(old_message, new_message) == "TODOOOO"
