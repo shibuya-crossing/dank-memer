9503f1e006b6afbe831465bbcf00a3a7aa23b56f
Ceravoux <102864012+Ceravoux@users.noreply.github.com>
2022-07-06 15:18:10 +0700

    Update test_emoji_match.py
    update test

--- tests/work/test_emoji_match.py
+++ tests/work/test_emoji_match.py
@@ -2,14 +2,16 @@ from dank_memer.work import emoji_match
 from discord import Button, Message, Component, ActionRow
 
 old_message = Message()
-old_message.contents = """Work for Discord Mod - Emoji Match - Look at the emoji closely!
-:thinking:"""
+old_message.contents = """Work for Discord Mod - Emoji Match - Look at the emoji closely!\n🤔"""
 
 new_message = Message()
 new_message.contents = """What was the emoji?"""
 new_message.components = [ActionRow()]
-new_message.components[0].children = [Button(emoji="")]
+new_message.components[0].children = [Button(emoji="😉"), Button(emoji="😉"),
+                                      Button(emoji="🤗"), Button(emoji="😀"),
+                                      Button(emoji="😁"), Button(emoji="🤔"),
+                                      Button(emoji="😁"), Button(emoji="😉")]
 
 
 def test_emoji_match():
-    assert emoji_match(old_message, new_message) == "TODOOOO"
+    assert emoji_match(old_message, new_message) == 5
