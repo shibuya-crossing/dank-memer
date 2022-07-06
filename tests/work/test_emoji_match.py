a54913aa206f5badc9568be4cc457a7ac389466b
Ceravoux <102864012+Ceravoux@users.noreply.github.com>
2022-07-06 15:55:04 +0700

    sigh

--- tests/work/test_emoji_match.py
+++ tests/work/test_emoji_match.py
@@ -2,13 +2,17 @@ from dank_memer.work import emoji_match
 
 class Message:
     pass
+
 class ActionRow:
     pass
+
 class Emoji:
     def __init__(self, name):
         self.name = name
+        
 class Button:
-    def __init__(self, emoji):
+    def __init__(self, *, label=None, emoji=None):
+        self.label = label
         self.emoji = Emoji(emoji)
       
 old_message = Message()
