50f824841fcee38a7b287d2b8913d403efa16ea8
0xfinder <0xfinder@protonmail.com>
2022-07-06 11:49:55 +0800

    untrack files contained in the .gitignore file

--- dank_memer/work.py
+++ dank_memer/work.py
@@ -8,10 +8,12 @@ tilde_regex = re.compile(r"`(.*)`")
 
 def color_match(old_msg, new_msg):
     msg = old_msg
-    msg.contents = msg.contents.lower()
+    msg.content = msg.content.lower()
     words_color = dict(
-        zip(tilde_regex.findall(msg.contents), colon_regex.findall(msg.contents))
+        zip(tilde_regex.findall(msg.content), colon_regex.findall(msg.content))
     )
+    word = tilde_regex.search(new_msg).group()
+    return words_color[word]
 
 
 def emoji_match(old_msg, new_msg):
@@ -19,15 +21,11 @@ def emoji_match(old_msg, new_msg):
 
 
 def repeat_order(old_msg, new_msg):
-    words = tilde_regex.findall(old_msg["contents"])
-    print(words)
-    # words = tilde_regex.findall(old_msg.contents)
+    words = tilde_regex.findall(old_msg.content)
     order = []
     for _, word in enumerate(words):
-        for btn_idx, btn in enumerate(new_msg["components"][0]["children"]):
-            # for btn_idx, btn in enumerate(new_msg.components[0].children):
-            # if btn.label == word:
-            if btn["label"] == word:
+        for btn_idx, btn in enumerate(new_msg.components[0].children):
+            if btn.label == word:
                 order.append(btn_idx)
                 break
 
